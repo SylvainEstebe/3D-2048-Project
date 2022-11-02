9895f6d1dc5776ca922d578202543d450f846fc5
Estebe Sylvain <sylvainestebe@192.168.0.15>
2022-11-02 15:44:00 +0100

    Création Interface Graphique Premières méthodes dans le controller (test) Lancement de l'interface graphique

--- src/jeuConsole/FXMLController.java
+++ src/jeuConsole/FXMLController.java
@@ -4,14 +4,20 @@ package jeuConsole;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -20,12 +26,111 @@ import javafx.fxml.Initializable;
  */
 public class FXMLController implements Initializable {
 
+    @FXML
+    private MenuItem newPartie;
+    @FXML
+    private MenuItem chargePartie;
+    @FXML
+    private MenuItem sauvegardePartie;
+    @FXML
+    private Button help;
+    @FXML
+    private Button stopIA;
+    @FXML
+    private Button undo;
+    @FXML
+    private GridPane grilleH;
+    @FXML
+    private GridPane grilleM;
+    @FXML
+    private Pane tuile;
+    @FXML
+    private Label valTuile;
+    @FXML
+    private GridPane grilleB;
+    @FXML
+    private Label meilleurScore;
+    @FXML
+    private Label score;
+    @FXML
+    private MenuItem ia1;
+    @FXML
+    private MenuItem i2;
+    @FXML
+    private MenuItem i3;
+    @FXML
+    private MenuItem stat;
+    @FXML
+    private Label appuie;
+
     /**
      * Initializes the controller class.
      */
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-         }
+        // TODO
+        
+    }
 
+    @FXML
+     /**
+     * Méthode qui affiches des aides concernant les commandes du jeu sous forme d'alert      *
+     * 
+     */
+    private void aidePopUp(MouseEvent event) {
+  Alert alert = new Alert(AlertType.WARNING);
+        alert.setTitle("Bievenue dans le 2048");
+        alert.setHeaderText("Quelques informations pour les commandes");
+        alert.setContentText("Presser le bouton D pour allez à droite, G pour allez à gauche, B pour allez en bas, H pour allez en haut, E pour le niveau supérieur et Q pour le niveau inférieur!");
+        alert.showAndWait();    }
+
+     /**
+     * Méthode qui lorsqu'on sélectionne une des ia dans le menu permet le déclenchement de l'algo assoscié
+     * Également rend visible le bouton stop IA
+     * 
+     */
+    @FXML
+    private void ia(ActionEvent event) {
+        stopIA.setDisable(false);
+    }
+
+    @FXML
+    /**
+     * Méthode qui affiches les statistiques dans l'interface      *
+     * 
+     */
+    private void afficheStat(ActionEvent event) {
+    }
+
+    @FXML
+    private void nouvellePartie(ActionEvent event) {
+    }
+
+    @FXML
+    private void chargerPartie(ActionEvent event) {
+    }
+
+    @FXML
+    private void sauvegarderPartie(ActionEvent event) {
+        //Lorsqu'on sauvegarde, le bouton de chargement devient actif
+        chargePartie.setDisable(false);
+    }
+
+    @FXML
+    private void undo(MouseEvent event) {
+    }
+
+     /**
+     * Prend la direction et en fonction ..*
+     * 
+     */
+    @FXML
+    private void Direction(KeyEvent event) {
+        String direction = event.getText();
+        appuie.setText("Tu as appuyé sur " + event.getText());
+        tuile.setTranslateX(100);
+        
+    }
+
+  
 }
--- src/jeuConsole/NewFXMain.java
+++ src/jeuConsole/NewFXMain.java
@@ -1,15 +1,16 @@
 package jeuConsole;
 
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
  */
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -22,30 +23,20 @@ import javafx.stage.Stage;
 public class NewFXMain extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+    public void start(Stage stage) throws IOException {
+Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
 
+Scene scene = new Scene(root);
+boolean add = scene.getStylesheets().add("css/styles.css");
+
+stage.setScene(scene);
+stage.show();
+}
     /**
      * @param args the command line arguments
      */
+    
+  
     public static void main(String[] args) {
         launch(args);
     }
