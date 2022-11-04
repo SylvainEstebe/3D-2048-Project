43b142050b4d5dca8f9ed3bb038935387e114d5a
Estebe Sylvain <sylvainestebe@192.168.0.15>
2022-11-04 16:01:35 +0100

    BDD

--- src/application/FXMLController.java
+++ src/application/FXMLController.java
@@ -4,10 +4,8 @@
  */
 package application;
 
-
 import java.net.URL;
 import java.util.ArrayList;
-
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -26,8 +24,9 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-
+import java.sql.*;
 import javafx.stage.Stage;
+import modele.ConnexionBDD;
 import modele.Jeu;
 import variables.Parametres;
 
@@ -38,7 +37,6 @@ import variables.Parametres;
  */
 public class FXMLController implements Initializable, Parametres {
 
-    
     @FXML
     private MenuItem chargePartie;
     @FXML
@@ -61,7 +59,7 @@ public class FXMLController implements Initializable, Parametres {
     private GridPane grilleB;
     @FXML
     private Label meilleurScore;
-    
+
     @FXML
     private MenuItem ia1;
     @FXML
@@ -70,77 +68,72 @@ public class FXMLController implements Initializable, Parametres {
     private MenuItem i3;
     @FXML
     private MenuItem stat;
-    
-    
-    
-    
-    
+
     @FXML
     private MenuItem newPartie;
-    
+
     private ArrayList<ArrayList<Label>> eltsGrilles;
-    
+
     private Jeu jeuAppli;
-    
+
     private ArrayList<GridPane> tabGrillesApp;
-    
+
     @FXML
     private Label score;
-    
+    @FXML
+    private Button boutonBDD;
+    @FXML
+    private Button boutonBDD2;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
         // TODO
-        
     }
+
     @FXML
     public void nouvellePartie(ActionEvent event) {
-        tabGrillesApp=new ArrayList<GridPane>();
+        tabGrillesApp = new ArrayList<GridPane>();
         tabGrillesApp.add(grilleH);
         tabGrillesApp.add(grilleM);
         tabGrillesApp.add(grilleB);
-        jeuAppli=new Jeu();
-        jeuAppli.lancementJeuAppli(); 
+        jeuAppli = new Jeu();
+        jeuAppli.lancementJeuAppli();
         this.majGrillesApp();
-        
+
     }
-    
-    private void majGrillesApp(){
+
+    private void majGrillesApp() {
         //Boucle pour chaque grille
-        for (int k=0;k<TAILLE;k++){
-           for (int i=0;i<TAILLE;i++){
-                for (int j=0;j<TAILLE;j++){
-                    
-                    Label caseJeu=new Label(""+jeuAppli.getGrilles().get(k).getGrille().get(j).get(i).getValeur());
+        for (int k = 0; k < TAILLE; k++) {
+            for (int i = 0; i < TAILLE; i++) {
+                for (int j = 0; j < TAILLE; j++) {
+
+                    Label caseJeu = new Label("" + jeuAppli.getGrilles().get(k).getGrille().get(j).get(i).getValeur());
                     caseJeu.getStyleClass().add("caseJeu");
-                    Pane caseJeuCouleur=new Pane();
-                    
-                    
+                    Pane caseJeuCouleur = new Pane();
+
                     //Gestion des bordures des grilles
-                    if(j!=2 && i!=2){
+                    if (j != 2 && i != 2) {
                         caseJeuCouleur.setStyle("-fx-border-width : 1px 0px 0px 1px ");
-                    }
-                    else if (j==2 && i!=2){
+                    } else if (j == 2 && i != 2) {
                         caseJeuCouleur.setStyle("-fx-border-width : 1px 0px 1px 1px ");
-                    }
-                    else if (j!=2 &&i==2){
+                    } else if (j != 2 && i == 2) {
                         caseJeuCouleur.setStyle("-fx-border-width : 1px 1px 0px 1px ");
-                        
-                    }
-                    else{
+
+                    } else {
                         caseJeuCouleur.setStyle("-fx-border-width : 1px 1px 1px 1px ");
                     }
-                    
+
                     //Gestion des couleurs des cases
-                    switch (jeuAppli.getGrilles().get(k).getGrille().get(j).get(i).getValeur()){
-                        case 2 :
+                    switch (jeuAppli.getGrilles().get(k).getGrille().get(j).get(i).getValeur()) {
+                        case 2:
                             caseJeuCouleur.setStyle("-fx-background-color : #FFFADF;");
                             break;
-                        case 4 :
+                        case 4:
                             caseJeuCouleur.setStyle("-fx-background-color : #F3E9BE;");
                             break;
                         case 8:
@@ -171,7 +164,7 @@ public class FXMLController implements Initializable, Parametres {
                             caseJeuCouleur.setStyle("-fx-background-color : #FFE76C;");
                             break;
                     }
-                    
+
                     caseJeuCouleur.getStyleClass().add("couleurCase");
                     caseJeu.setVisible(true);
                     caseJeuCouleur.setVisible(true);
@@ -179,64 +172,62 @@ public class FXMLController implements Initializable, Parametres {
                     tabGrillesApp.get(k).add(caseJeuCouleur, i, j);
                     tabGrillesApp.get(k).add(caseJeu, i, j);
                 }
-            } 
-          
-        }
-            
-    }
-    
+            }
 
-    
-    
-    
-    
-    
+        }
+
+    }
+
     @FXML
-     /**
-     * Méthode qui affiches des aides concernant les commandes du jeu sous forme d'alert      *
-     * 
+    /**
+     * Méthode qui affiches des aides concernant les commandes du jeu sous forme
+     * d'alert *
+     *
      */
     private void aidePopUp(MouseEvent event) {
-        Stage fenetre_aide=new Stage();
+        Stage fenetre_aide = new Stage();
         fenetre_aide.setTitle("Comment jouer?");
-        BorderPane root = new BorderPane(); 
+        BorderPane root = new BorderPane();
         root.getStyleClass().add("pane");
-        
+
         //Titre
-        Label titre_aide=new Label("Règles du jeu");
+        Label titre_aide = new Label("Règles du jeu");
         titre_aide.getStyleClass().add("text_horsjeu");
         root.setTop(titre_aide);
-        
+
         //Texte explicatif
-        Pane boxText_expli=new Pane();
+        Pane boxText_expli = new Pane();
         boxText_expli.getStyleClass().add("boxStyle1");
         boxText_expli.setPrefWidth(400);
-        
-        Label text_expli=new Label(" Presser D pour aller à droite"
-                + "\n Presser G pour aller à gauche."+"\n Presser H pour aller en haut."
+
+        Label text_expli = new Label(" Presser D pour aller à droite"
+                + "\n Presser G pour aller à gauche." + "\n Presser H pour aller en haut."
                 + "\n Presser B pour aller en bas.\n Presser E pour aller au niveau supérieur."
                 + "\n Presser Q pour aller au niveau inférieur.");
         text_expli.getStyleClass().add("text_horsjeu2");
         boxText_expli.getChildren().add(text_expli);
         root.setLeft(boxText_expli);
-        root.setMargin(boxText_expli, new Insets(10,10,10,10));
-        
-        final Scene scene = new Scene(root, 500, 200); 
+        root.setMargin(boxText_expli, new Insets(10, 10, 10, 10));
+
+        final Scene scene = new Scene(root, 500, 200);
         boolean add = scene.getStylesheets().add("css/style.css");
-        fenetre_aide.setScene(scene); 
+        fenetre_aide.setScene(scene);
         fenetre_aide.show();
-        
-        /**Alert alert = new Alert(AlertType.WARNING);
-        alert.setTitle("Bievenue dans le 2048");
-        alert.setHeaderText("Quelques informations pour les commandes");
-        alert.setContentText("Presser le bouton D pour allez à droite, G pour allez à gauche, B pour allez en bas, H pour allez en haut, E pour le niveau supérieur et Q pour le niveau inférieur!");
-        alert.showAndWait(); **/   
+
+        /**
+         * Alert alert = new Alert(AlertType.WARNING); alert.setTitle("Bievenue
+         * dans le 2048"); alert.setHeaderText("Quelques informations pour les
+         * commandes"); alert.setContentText("Presser le bouton D pour allez à
+         * droite, G pour allez à gauche, B pour allez en bas, H pour allez en
+         * haut, E pour le niveau supérieur et Q pour le niveau inférieur!");
+         * alert.showAndWait(); *
+         */
     }
 
-     /**
-     * Méthode qui lorsqu'on sélectionne une des ia dans le menu permet le déclenchement de l'algo assoscié
-     * Également rend visible le bouton stop IA
-     * 
+    /**
+     * Méthode qui lorsqu'on sélectionne une des ia dans le menu permet le
+     * déclenchement de l'algo assoscié Également rend visible le bouton stop IA
+     *
      */
     @FXML
     private void ia(ActionEvent event) {
@@ -245,13 +236,11 @@ public class FXMLController implements Initializable, Parametres {
 
     @FXML
     /**
-     * Méthode qui affiches les statistiques dans l'interface      *
-     * 
+     * Méthode qui affiches les statistiques dans l'interface *
+     *
      */
     private void afficheStat(ActionEvent event) {
     }
-
-    
 
     @FXML
     private void chargerPartie(ActionEvent event) {
@@ -267,90 +256,123 @@ public class FXMLController implements Initializable, Parametres {
     private void undo(MouseEvent event) {
     }
 
-    public void majScoreApp(){
-        score.setText(""+jeuAppli.getScoreFinal());
+    public void majScoreApp() {
+        score.setText("" + jeuAppli.getScoreFinal());
         score.setVisible(true);
     }
-
 
     @FXML
     private void mouvJoueur(KeyEvent event) {
         String direction = event.getText();
-        boolean b=false;
+        boolean b = false;
         //Déplacement des cases selon la touche clavier
-        if(direction.equals("g")){
-            b=jeuAppli.deplacerCases3G(GAUCHE);
+        if (direction.equals("g")) {
+            b = jeuAppli.deplacerCases3G(GAUCHE);
+        } else if (direction.equals("d")) {
+            b = jeuAppli.deplacerCases3G(DROITE);
+        } else if (direction.equals("h")) {
+            b = jeuAppli.deplacerCases3G(HAUT);
+        } else if (direction.equals("b")) {
+            b = jeuAppli.deplacerCases3G(BAS);
+        } else if (direction.equals("q")) {
+            b = jeuAppli.deplacerCases3G(DESCG);
+        } else if (direction.equals("e")) {
+            b = jeuAppli.deplacerCases3G(MONTERG);
         }
-        else if(direction.equals("d")){
-            b=jeuAppli.deplacerCases3G(DROITE);  
-        }
-        else if(direction.equals("h")){
-            b=jeuAppli.deplacerCases3G(HAUT); 
-        }
-        else if(direction.equals("b")){
-            b=jeuAppli.deplacerCases3G(BAS);  
-        }
-        else if(direction.equals("q")){
-            b=jeuAppli.deplacerCases3G(DESCG); 
-        }
-        else if(direction.equals("e")){
-            b=jeuAppli.deplacerCases3G(MONTERG);
-        }
-        
+
         jeuAppli.choixNbCasesAjout(b);
         this.majScoreApp();
         this.majGrillesApp();
-        if(jeuAppli.finJeu()){
+        if (jeuAppli.finJeu()) {
             if (jeuAppli.getValeurMaxJeu() >= OBJECTIF) {
                 this.victoireAppli();
-            } 
-            else {
+            } else {
                 this.jeuPerduAppli();
             }
         }
-        
+
     }
 
-    public void victoireAppli(){
-        Stage fenetre_aide=new Stage();
+    public void victoireAppli() {
+        Stage fenetre_aide = new Stage();
         fenetre_aide.setTitle("VICTOIRE");
-        BorderPane root = new BorderPane(); 
+        BorderPane root = new BorderPane();
         root.getStyleClass().add("pane");
-        
+
         //Titre
-        Label victoire=new Label("Félicitations, vous avez gagné!");
-        Label scoreAff=new Label("Votre score : "+jeuAppli.getScoreFinal());
+        Label victoire = new Label("Félicitations, vous avez gagné!");
+        Label scoreAff = new Label("Votre score : " + jeuAppli.getScoreFinal());
         victoire.getStyleClass().add("text_horsjeu");
         scoreAff.getStyleClass().add("text_horsjeu");
         root.setCenter(victoire);
         root.setTop(scoreAff);
-        final Scene scene = new Scene(root, 100, 100); 
+        final Scene scene = new Scene(root, 100, 100);
         boolean add = scene.getStylesheets().add("css/style.css");
-        fenetre_aide.setScene(scene); 
+        fenetre_aide.setScene(scene);
         fenetre_aide.show();
     }
-    
-    public void jeuPerduAppli(){
-        Stage fenetre_aide=new Stage();
+
+    public void jeuPerduAppli() {
+        Stage fenetre_aide = new Stage();
         fenetre_aide.setTitle("DEFAITE");
-        BorderPane root = new BorderPane(); 
+        BorderPane root = new BorderPane();
         root.getStyleClass().add("pane");
-        
+
         //Titre
-        Label defaite=new Label("Mince, vous avez perdu!");
-        Label scoreAff=new Label("Votre score : "+jeuAppli.getScoreFinal());
+        Label defaite = new Label("Mince, vous avez perdu!");
+        Label scoreAff = new Label("Votre score : " + jeuAppli.getScoreFinal());
         defaite.getStyleClass().add("text_horsjeu");
         scoreAff.getStyleClass().add("text_horsjeu");
         root.setCenter(defaite);
         root.setTop(scoreAff);
-        final Scene scene = new Scene(root, 100, 100); 
+        final Scene scene = new Scene(root, 100, 100);
         boolean add = scene.getStylesheets().add("css/style.css");
-        fenetre_aide.setScene(scene); 
+        fenetre_aide.setScene(scene);
         fenetre_aide.show();
     }
-    
-  
-    
+
+    // Enregistrement lors du clic sur le bouton BDD
+    @FXML
+    private void enregistrementBDD(MouseEvent event) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+        // ça serait chouette de demander lors d'une nouvelle partie le pseudo du joueur
+        // Le pseudo en dessous est juste pour le test
+        String pseudo = "Sylvain";
+
+        // Stockage du score du joueur
+        int scoreJoueurFin = Integer.valueOf(score.getText());
+        
+        // Récupérer les données de score, temps (existe pas encors) et déplacement (existe pas encore)
+        
+        // BDD infos
+        String host = "localhost";
+        String port = "8889";
+        String dbname = "2048_game_Estebe";
+        String username = "java";
+        String password = "projetjava";
+        // BDD Connexion
+        ConnexionBDD c = new ConnexionBDD(host, port, dbname, username, password);
+        // BDD requête ajout Joueur(int,string,int,int,int) Joueur(id,pseudo,score,temps,deplacement)
+        String query = "INSERT INTO Joueur VALUES ('"+ 0 +"','"+ pseudo + "','" + scoreJoueurFin + "','" + 0 + "','" + 0 + "')";
+        c.insertTuples(query);
+
+    }
+
+    @FXML
+    private void consultationBDD(MouseEvent event) {
+        String host = "localhost";
+        String port = "8889";
+        String dbname = "2048_game_Estebe";
+        String username = "java";
+        String password = "projetjava";
+        ConnexionBDD c = new ConnexionBDD(host, port, dbname, username, password);
+
+        // On faira différentes méthodes en fonction des besoins et différentes conditions
+        String queryScore = "SELECT  pseudo, scores FROM Joueur ORDER BY scores DESC ";
+        String queryTemps = "SELECT  pseudo, temps FROM Joueur ORDER BY temps ASC";
+        String queryDéplacement = "SELECT  pseudo, nombreDéplacement FROM Joueur ORDER BY nombreDéplacement ASC ";
+        c.getTuples(queryScore);
+        
+    }
+
 }
-
-
